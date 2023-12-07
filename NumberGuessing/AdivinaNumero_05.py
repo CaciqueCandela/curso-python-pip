5445955db132c5851f2ccb5a2d47d8b9775a3ea3
Nelson <jaguar1077@gmail.com>
2023-12-06 20:08:19 -0500

    Mi primer commit en este archivo

---
+++ NumberGuessing/AdivinaNumero_05.py
@@ -0,0 +1,35 @@
+'''
+Programa para adivinar un número. => La computadora adivina nuestro número secreto (Computer guessing our secret number).
+'''
+
+import random
+
+
+def computer_guess(x):
+
+    low = 1
+    high = x
+    feedback = '' #Ingresamos una cadena (str) vacía.
+
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+
+        else:
+            guess = low # Could also be high b/c low = high
+
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+            
+        elif feedback == 'l':
+            low = guess + 1
+        
+    print(f'Yay! The computer guessed your number, {guess}, correctly!!')
+
+# computer_guess(1000)
+
+# => si vamos a agregar el Entry Point:
+
+if __name__ == '__main__':
+    computer_guess(1000)
