256e2df7b2fbc07ff91a3c8b43064cf255466c31
Nelson <jaguar1077@gmail.com>
2023-12-22 16:47:51 -0500

    Clase: Pandas

--- app/main2.py
+++ app/main2.py
@@ -1,20 +1,22 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
   '''
-  Código para generar el pie chart sin usar pandas.
+  Código para generar el pie chart usando Pandas.
   '''
 
+  df = pd.read_csv('data.csv')
+  df2 = df[df['Continent'] == 'Africa']
+
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+  
+
   data = read_csv.read_csv('data.csv')
-  data = list(filter(lambda item : item['Continent'] == 'South America',data)) 
-  # Filtramos por la columna 'Continent' y de ahí, 'South America'.
-  
-  countries = list(map(lambda x: x['Country'], data)) # Traemos la información de todos los países.
-  percentages = list(map(lambda x: x['World Population Percentage'], data))  
-  charts.generate_pie_chart(countries, percentages) # Traemos los porcentajes para realizar un 'Chart_pie'.
-  
   country = input('Type Country => ')
   print(country)
 
